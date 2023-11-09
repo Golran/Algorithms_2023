0141365e09a586dad05afecce087f7977875c315
Еремин Илья Викторович <i.eremin@kontur>
2023-11-09 11:25:09 +0500

    Init

---
+++ Algorithms/RecursionAlgorithms.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Algorithms;
+
+public static class RecursionAlgorithms
+{
+	public static double MathPow(double num, int deg)
+	{
+		return Math.Pow(num, deg);
+	}
+
+	public static double TrivialPow(double num, int deg)
+	{
+		// Простое умножение в цикле
+		return 0.0;
+	}
+
+	public static double FastRecPow(double num, int deg)
+	{
+		// Алгоритм быстрого возведения в степень (Рекурсивный)
+		return 0.0;
+	}
+
+	public static double FastCyclePow(double num, int deg)
+	{
+		// Алгоритм быстрого возведения в степень (Рекурсивный)
+		return 0.0;
+	}
+}
